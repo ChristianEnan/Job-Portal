271ae3f372e604d12bfcad6ce4aae1533c9055ee
Enan Christiann <christianenan57@gmail.com>
2026-08-17 12:57:48 +0530

    feat: Enhance company form with improved styling and validation; update job detail page layout

--- JobPortal/jobs/forms.py
+++ JobPortal/jobs/forms.py
@@ -15,6 +15,40 @@ class CompanyForm(forms.ModelForm):
             "description",
         ]
 
+        widgets = {
+
+            "name": forms.TextInput(attrs={
+                "class": "form-control",
+                "placeholder": "Enter Company Name",
+            }),
+
+            "website": forms.URLInput(attrs={
+                "class": "form-control",
+                "placeholder": "https://example.com",
+            }),
+
+            "email": forms.EmailInput(attrs={
+                "class": "form-control",
+                "placeholder": "company@email.com",
+            }),
+
+            "location": forms.TextInput(attrs={
+                "class": "form-control",
+                "placeholder": "Ahmedabad, India",
+            }),
+
+            "description": forms.Textarea(attrs={
+                "class": "form-control",
+                "rows": 5,
+                "placeholder": "Write company description...",
+            }),
+
+            "logo": forms.ClearableFileInput(attrs={
+                "class": "form-control",
+            }),
+
+        }
+
 class JobForm(forms.ModelForm):
 
     class Meta:
